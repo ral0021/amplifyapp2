ea415719713fd474d72386d51a8a5cedc59de262
Ryan <ral0021@mix.wvu.edu>
2021-03-18 21:37:26 -0400

    fixed buttons on add medication

--- src/components/Add_medication.js
+++ src/components/Add_medication.js
@@ -96,17 +96,29 @@ const Add_medication = () => {
 
     <div class="row justify-content-center pb-5">
             <div class="col-xl-2 col-lg-3 col-12">
-                <button class="menu-button btn btn-light my-2" ><a href="/current_medication">Current Medication</a></button>
+                <button class="menu-button btn btn-light my-2"  onClick={(e) => {
+      e.preventDefault();
+      window.location.href='/current_medication';
+      }} >Current Medication</button>
             </div>
             <div class="col-xl-2 col-lg-3 col-12">
-                <button class="menu-button btn btn-light my-2" ><a href="/pill_information">Pill Information</a></button>
+                <button class="menu-button btn btn-light my-2" onClick={(e) => {
+      e.preventDefault();
+      window.location.href='/pill_information';
+      }} >Pill Information</button>
             </div>
             <div class="col-xl-2 col-lg-3 col-12">
-                <button class="menu-button btn btn-dark my-2" ><a href="/">Main Menu</a></button>
+                <button class="menu-button btn btn-dark my-2"  onClick={(e) => {
+      e.preventDefault();
+      window.location.href='/';
+      }} > Main Menu</button>
             </div>
         </div>
     </div>
     
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" 
+crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
     </div>
